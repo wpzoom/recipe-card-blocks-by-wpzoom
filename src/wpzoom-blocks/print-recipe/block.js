cebf387a0252fc9e62542589187164feb8cf11b5
ciorici <cioricip@gmail.com>
2019-03-19 11:05:26 +0100

    gitinore + new files

---
+++ src/wpzoom-blocks/print-recipe/block.js
@@ -0,0 +1,76 @@
+/**
+ * BLOCK: block-print-recipe
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+/* External dependencies */
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+
+/**
+ * Register: Ingredients Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'wpzoom-recipe-card/block-print-recipe', {
+    // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+    title: __( "Print Recipe", "wpzoom-recipe-card" ), // Block title.
+    description: __( "A button to print WPZOOM Recipe Card.", "wpzoom-recipe-card" ),
+    icon: {
+        // Specifying a background color to appear with the icon e.g.: in the inserter.
+        background: '#2EA55F',
+        // Specifying a color for the icon (optional: if not set, a readable color will be automatically defined)
+        foreground: '#fff',
+        // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+        src: 'editor-insertmore',
+    },
+    category: 'wpzoom-recipe-card', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+    // Allow only one Recipe Card Block per post.
+    supports: {
+        multiple: false,
+        html: false
+    },
+    keywords: [
+        __( "Recipe Card", "wpzoom-recipe-card" ),
+        __( "Block Recipe Card", "wpzoom-recipe-card" ),
+        __( "WPZOOM", "wpzoom-recipe-card" ),
+    ],
+
+    /**
+     * The edit function describes the structure of your block in the context of the editor.
+     * This represents what the editor will render when the block is used.
+     *
+     * The "edit" property must be a valid function.
+     *
+     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+     */
+    edit: ( { attributes, setAttributes, className, clientId } ) => {
+        const { id, text } = attributes;
+
+        return (
+            <Fragment>
+                <a href={ `#${ id }` } className={ className }>{ text }</a>
+            </Fragment>
+        );
+    },
+
+    save() {
+        // Rendering in PHP
+        return null;
+    }
+
+} );
+
+
+
