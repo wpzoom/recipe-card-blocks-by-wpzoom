6020e57c8638ed825d013790ab0c6f07d62e0432
vicolaspetru <vicolaspetru@gmail.com>
2019-11-26 13:32:19 +0200

    Fixed issue overlap pinterest and print icon

--- src/wpzoom-blocks/recipe-card/components/RecipeCard.js
+++ src/wpzoom-blocks/recipe-card/components/RecipeCard.js
@@ -226,7 +226,7 @@ export default class RecipeCard extends Component {
                                                 pin_btn &&
                                                 <div className={ PinterestClasses }>
                                                     <a className="btn-pinit-link no-print" data-pin-do="buttonPin" href={ pinitURL } data-pin-custom="true">
-                                                        <i className="fab fa-pinterest-p icon-pinit-link"></i>
+                                                        <i className="icon-pinit-link"></i>
                                                         <span>{ __( "Pin", "wpzoom-recipe-card" ) }</span>
                                                     </a>
                                                 </div>
@@ -235,7 +235,7 @@ export default class RecipeCard extends Component {
                                                 print_btn &&
                                                 <div className={ PrintClasses }>
                                                     <a className="btn-print-link no-print" href={ "#" + id } title={ __( "Print directions...", "wpzoom-recipe-card" ) }>
-                                                        <i className="fas fa-print icon-print-link"></i>
+                                                        <i className="icon-print-link"></i>
                                                         <span>{ __( "Print", "wpzoom-recipe-card" ) }</span>
                                                     </a>
                                                 </div>
@@ -313,7 +313,7 @@ export default class RecipeCard extends Component {
                                                 pin_btn &&
                                                 <div className={ PinterestClasses }>
                                                     <a className="btn-pinit-link no-print" data-pin-do="buttonPin" href={ pinitURL } data-pin-custom="true">
-                                                        <i className="fab fa-pinterest-p icon-pinit-link"></i>
+                                                        <i className="icon-pinit-link"></i>
                                                         <span>{ __( "Pin", "wpzoom-recipe-card" ) }</span>
                                                     </a>
                                                 </div>
@@ -322,7 +322,7 @@ export default class RecipeCard extends Component {
                                                 print_btn &&
                                                 <div className={ PrintClasses }>
                                                     <a className="btn-print-link no-print" href={ "#" + id } title={ __( "Print directions...", "wpzoom-recipe-card" ) }>
-                                                        <i className="fas fa-print icon-print-link"></i>
+                                                        <i className="icon-print-link"></i>
                                                         <span>{ __( "Print", "wpzoom-recipe-card" ) }</span>
                                                     </a>
                                                 </div>
