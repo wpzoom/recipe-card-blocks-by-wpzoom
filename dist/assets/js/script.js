929e6bde5e6d12a33002f4f4638dd270383321ab
vicolaspetru <vicolaspetru@gmail.com>
2021-05-20 12:56:24 +0300

    Add script to make embeds responsive so they don't overflow recipe card container

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -142,3 +142,42 @@
         } );
     } );
 }( jQuery, wpzoomRecipeCard ) );
+
+/**
+ * Make embeds responsive so they don't overflow their container.
+ * Add max-width & max-height to <iframe> elements, depending on their width & height props.
+ *
+ * @see Twenty Twenty-One file responsive-embeds.js.
+ * @since 2.7.8
+ *
+ * @return {void}
+ */
+ function recipeCardResponsiveEmbeds() {
+	let proportion, parentWidth;
+
+	// Loop iframe elements.
+	document.querySelectorAll( 'iframe' ).forEach( function ( iframe ) {
+		// Only continue if the iframe has a width & height defined.
+		if ( iframe.width && iframe.height ) {
+			// Calculate the proportion/ratio based on the width & height.
+			proportion =
+				parseFloat( iframe.width ) / parseFloat( iframe.height );
+			// Get the parent element's width.
+			parentWidth = parseFloat(
+				window
+					.getComputedStyle( iframe.parentElement, null )
+					.width.replace( 'px', '' )
+			);
+			// Set the max-width & height.
+			iframe.style.maxWidth = '100%';
+			iframe.style.maxHeight =
+				Math.round( parentWidth / proportion ).toString() + 'px';
+		}
+	} );
+}
+
+// Run on initial load.
+recipeCardResponsiveEmbeds();
+
+// Run on resize.
+window.onresize = recipeCardResponsiveEmbeds;
