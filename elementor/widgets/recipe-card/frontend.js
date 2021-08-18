24a2cf42d50994eb0c973c59e21dc8a492bb9e69
Alexei Samarschi <Samarschi@gmail.com>
2021-08-18 17:19:16 +0300

    Fixed issue with print and multiple recipe cards

--- elementor/widgets/recipe-card/frontend.js
+++ elementor/widgets/recipe-card/frontend.js
@@ -1,9 +1,8 @@
 
 jQuery(document).ready(function ($) {
-
 	$('.elementor-rcb-print-button').on( 'click', function(e) {
 		e.preventDefault();
-		$('#wpzoom-recipe-card').print({
+		$( this ).closest('.wp-block-wpzoom-recipe-card-block-recipe-card').print({
 			globalStyles: true,
 			iframe: true,
 			stylesheet: wpzoomRecipeCardPrint.stylesheetPrintURL,
