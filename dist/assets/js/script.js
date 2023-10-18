96b74dc440ddb42028d4ee10e09a076b3445b3a2
Alexei Samarschi <samarschi@gmail.com>
2023-10-18 12:12:47 +0300

    Update script.js
    Fixed issue with print button in "Insert existing recipe" block

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -13,6 +13,7 @@
 		$('.wpzoom-custom-recipe-card-post').each(function( index ) {
 			var recipe_id = $(this).data('recipe-post');
 			$('.wpzoom-recipe-snippet-button.wp-block-wpzoom-recipe-card-block-print-recipe').attr( 'data-recipe-id', recipe_id );
+			$(this).find('.btn-print-link').attr( 'data-recipe-id', recipe_id );
 		});
 
 	}
