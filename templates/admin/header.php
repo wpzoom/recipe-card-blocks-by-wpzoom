6588353902758f420afd94fa2a46324a8e60d609
Pavel Ciorici <cioricip@gmail.com>
2024-05-28 12:21:14 +0200

    Update header.php

--- templates/admin/header.php
+++ templates/admin/header.php
@@ -9,7 +9,7 @@
 
 <div class="wpzoom-rcb-admin-panel-header">
 	<div class="wpzoom-rcb-admin-panel-header-inner">
-		<h3>Recipe Card Blocks PRO</h3>
+		<h3>Recipe Card Blocks</h3>
 		<h5>Import Recipes</h5>
 	</div>
 	<div class="wpzoom-rcb-plugin-version">v <?php echo WPZOOM_RCB_VERSION; ?></div>
