48634b9b5e9a1ac0e847e104d70e2648316016b4
Pavel Ciorici <cioricip@gmail.com>
2026-08-08 11:58:10 +0200

    Update footer.php

--- templates/admin/footer.php
+++ templates/admin/footer.php
@@ -11,9 +11,9 @@
 	<div class="wpzoom-rcb-admin-panel-footer-inner">
 		<img src="<?php echo untrailingslashit( WPZOOM_RCB_PLUGIN_URL ) . '/dist/assets/images/logo.svg'; ?>" alt="Recipe Card Blocks PRO" />
 		<ul class="wpzoom-rcb-footer-links">
-			<li><a href="https://recipecard.io/blog" target="_blank">Our Blog</a></li>
-			<li><a href="https://recipecard.io/support" target="_blank">Support</a></li>
-            <li><a href="https://recipecard.io/roadmap" target="_blank">Roadmap</a></li>
+			<li><a href="https://recipecard.io/blog" target="_blank"><?php esc_html_e( 'Our Blog', 'recipe-card-blocks-by-wpzoom' ); ?></a></li>
+			<li><a href="https://recipecard.io/support" target="_blank"><?php esc_html_e( 'Support', 'recipe-card-blocks-by-wpzoom' ); ?></a></li>
+			<li><a href="https://recipecard.io/roadmap" target="_blank"><?php esc_html_e( 'Roadmap', 'recipe-card-blocks-by-wpzoom' ); ?></a></li>
 		</ul>
 	</div>
 </div>
